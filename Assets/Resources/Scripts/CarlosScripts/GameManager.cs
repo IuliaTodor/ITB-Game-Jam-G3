ab916fe8a15f99125671e5179f68e0684850ae0b
Agustín Kohan <agustinkohanlopez@gmail.com>
2024-05-25 19:50:28 +0200

    Changes in spawn Enemy

--- Assets/Resources/Scripts/CarlosScripts/GameManager.cs
+++ Assets/Resources/Scripts/CarlosScripts/GameManager.cs
@@ -9,14 +9,6 @@ public class GameManager : MonoBehaviour
     public int score = 0;
     public int MipsAlive;
     public string playerName;
-    [SerializeField] private EnemySpawner _spawner;
-
-    [Space, Header("Spawn Time Related")]
-    [SerializeField] private int _minScore;
-    [SerializeField] private int _maxScore;
-    [SerializeField] private float _currentTimer;
-    [SerializeField] private float _minSpawnTime;
-    [SerializeField] private float _maxSpawnTime;
 
     // Start is called before the first frame update
     private void Awake()
@@ -34,24 +26,6 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         MipsAlive = EntityManager.Instance.MipsAlive.Count;
-    }
-
-    private void Update()
-    {
-        float t = Mathf.InverseLerp(_maxScore, _minScore, score);
-        float SpawnTime = Mathf.Lerp(_minSpawnTime, _maxSpawnTime, t);
-        _currentTimer += Time.deltaTime;
-
-        if (_currentTimer >= SpawnTime)
-        {
-            SpawnEnemy();
-            _currentTimer = 0;
-        }
-    }
-
-    private void SpawnEnemy()
-    {
-        _spawner.SpawnEnemy();
     }
 
     public void GameOver()
--- Assets/Resources/Scripts/agustinScripts/Enemy/Spawner/EnemySpawner.cs
+++ Assets/Resources/Scripts/agustinScripts/Enemy/Spawner/EnemySpawner.cs
@@ -1,9 +1,29 @@
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] float _radius;
     [SerializeField] GameObject _enemyPrefab;
+
+    [Space, Header("Spawn Time Related")]
+    [SerializeField] private int _minScore;
+    [SerializeField] private int _maxScore;
+    [SerializeField] private float _currentTimer;
+    [SerializeField] private float _minSpawnTime;
+    [SerializeField] private float _maxSpawnTime;
+    private void Update()
+    {
+        float t = Mathf.InverseLerp(_maxScore, _minScore, score);
+        float SpawnTime = Mathf.Lerp(_minSpawnTime, _maxSpawnTime, t);
+        _currentTimer += Time.deltaTime;
+
+        if (_currentTimer >= SpawnTime)
+        {
+            SpawnEnemy();
+            _currentTimer = 0;
+        }
+    }
 
     [ContextMenu("SpawnEnemy")]
     public void SpawnEnemy()
